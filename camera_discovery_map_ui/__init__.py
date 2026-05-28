3ed728957defe305228466ddbe6b53267c8eef54
dshipley71 <108143903+dshipley71@users.noreply.github.com>
2026-05-28 15:47:44 -0400

    Delete camera_discovery_map_ui directory

--- camera_discovery_map_ui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Standalone map/review UI tooling for camera-discovery artifacts."""
-
-from camera_discovery_map_ui.agent import CameraDiscoveryMapAgent, MapBuildResult
-
-__all__ = ["CameraDiscoveryMapAgent", "MapBuildResult"]
-__version__ = "0.1.0"
